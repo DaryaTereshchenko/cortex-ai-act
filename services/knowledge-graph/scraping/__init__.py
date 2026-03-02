3e7f1dcc1e19f51e924078a901418a55cc565e2b
DaryaTereshchenko <dara.stetsenko97@gmail.com>
2026-03-02 20:45:47 +0100

    lint compliance

--- services/knowledge-graph/scraping/__init__.py
+++ services/knowledge-graph/scraping/__init__.py
@@ -12,6 +12,7 @@ from .downloading import (
     get_html_content,
     get_urls,
 )
+from .extractor import BaseDocumentExtractor
 from .models import (
     Annex,
     Article,
@@ -24,55 +25,51 @@ from .models import (
     Section,
     classify_link,
 )
-from .schemas import (
-    AI_ACT_SCHEMA,
-    DSA_SCHEMA,
-    ParsingSchema,
-    SCHEMA_REGISTRY,
-    get_schema,
-)
-from .extractor import BaseDocumentExtractor
 from .parsers import (
     AIActExtractor,
     DSAExtractor,
-    EURLexExtractor,  # backward-compat alias
+    EURLexExtractor,
     create_extractor,
     extract_from_html,
     extract_from_url,
     register_parser,
 )
+from .schemas import (
+    AI_ACT_SCHEMA,
+    DSA_SCHEMA,
+    SCHEMA_REGISTRY,
+    ParsingSchema,
+    get_schema,
+)
 
 __all__ = [
-    # Downloading
-    "ScrapingError",
-    "get_ai_act_content",
-    "get_dsa_content",
-    "get_html_content",
-    "get_urls",
-    # Data models
+    "AI_ACT_SCHEMA",
+    "AIActExtractor",
     "Annex",
     "Article",
+    "BaseDocumentExtractor",
     "Chapter",
     "CrossReference",
+    "DSA_SCHEMA",
+    "DSAExtractor",
     "Document",
+    "EURLexExtractor",
     "LinkType",
     "Paragraph",
+    "ParsingSchema",
     "Recital",
+    "SCHEMA_REGISTRY",
+    "ScrapingError",
     "Section",
     "classify_link",
-    # Schemas
-    "AI_ACT_SCHEMA",
-    "DSA_SCHEMA",
-    "ParsingSchema",
-    "SCHEMA_REGISTRY",
-    "get_schema",
-    # Extractors
-    "BaseDocumentExtractor",
-    "AIActExtractor",
-    "DSAExtractor",
-    "EURLexExtractor",
     "create_extractor",
     "extract_from_html",
     "extract_from_url",
+    "get_ai_act_content",
+    "get_dsa_content",
+    "get_html_content",
+    "get_schema",
+    "get_urls",
     "register_parser",
 ]
+
